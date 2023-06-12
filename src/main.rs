1088cec27526a187fe3d06bf21fe8402487bb26d
Reuben Wong <reuben.wong.ys@gmail.com>
2023-06-13 01:33:25 +0800

    skeleton for interpreter spawn

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
-#![allow(missing_docs)]
-#![allow(rustdoc::missing_doc_code_examples)]
+use std::path::PathBuf;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about=None)]
+struct Args {
+    source_code: Option<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if let Some(source_code) = &args.source_code {
+        println!("Running rlox interpreter on {:?}", source_code);
+    } else {
+        println!("Running rlox prompt");
+    }
 }
